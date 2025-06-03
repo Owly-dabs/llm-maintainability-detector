197913e06efe18f7bee0b0d6c1d9784e6410b04c
Owly-dabs <owlycodes@gmail.com>
2025-06-03 16:53:53 -0700

    add CLI support and remove hardcoded file location

--- main.py
+++ main.py
@@ -2,15 +2,19 @@
 from scripts import evaluate, process_traits as pt
 
 import json
+import argparse
 
 def main():
-    CODE_FILE = "example_code2.py"
+    parser = argparse.ArgumentParser(description="Evaluate code traits.")
+    parser.add_argument("code_file", help="The code file to evaluate")
+    args = parser.parse_args()
+
+    CODE_FILE = args.code_file
     
-    with open(f"examples/{CODE_FILE}") as f:
+    with open(f"{CODE_FILE}") as f:
         code = f.read()
     
     language = evaluate.detect_language_from_filename(CODE_FILE)
-    print(language)
         
     traits_response = evaluate.evaluate_all_traits(
         language=language, 
@@ -19,10 +23,12 @@ def main():
     )
     
     issues = pt.build_issues_from_single_response(traits_response)
+    
+    filename = CODE_FILE.split("/")[-1].split(".")[0]
 
-    with open(f"example_outputs/{CODE_FILE}.json", "w") as out:
+    with open(f"example_outputs/{filename}.json", "w") as out:
         json.dump(issues, out, indent=2)
-        print(f"\n✅ Saved {len(issues)} issue(s) to issues_output.json")
+        print(f"\n✅ Saved {len(issues)} issue(s) to example_output/{filename}.json")
 
 
 if __name__=="__main__":
